7a022d94ece44e66d5a0dd205c2f9a5d2c116148
Michael Smith <michaels@miec.com>
2019-01-30 12:46:06 -0800

    added Claims processor classifier

--- run_classifier.py
+++ run_classifier.py
@@ -362,6 +362,50 @@ class ColaProcessor(DataProcessor):
           InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
     return examples
 
+class ClaimsProcessor(DataProcessor):
+  """Processor for the Claims Incident Description / Allegation Sentiment Analysis."""
+
+  def get_train_examples(self, data_dir):
+    """See base class."""
+    return self._create_examples(
+        self._read_tsv(os.path.join(data_dir, "train.tsv")), "train")
+
+  def get_dev_examples(self, data_dir):
+    """See base class."""
+    return self._create_examples(
+        self._read_tsv(os.path.join(data_dir, "dev.tsv")), "dev")
+
+  def get_test_examples(self, data_dir):
+    """See base class."""
+    return self._create_examples(
+        self._read_tsv(os.path.join(data_dir, "test.tsv")), "test")
+
+  def get_labels(self):
+    """See base class."""
+    return ["0", "1"]
+
+  def _create_examples(self, lines, set_type):
+    """Creates examples for the training and dev sets."""
+    examples = []
+    for (i, line) in enumerate(lines):
+      # no sets have a header
+      if i == -1:
+        continue
+      guid = "%s-%s" % (set_type, i)
+      if set_type == "test":
+        # in test mode, the index is line[0] and sentence is line[1] 
+        text_a = tokenization.convert_to_unicode(line[1])
+        text_b = tokenization.convert_to_unicode(line[2])
+        label = "0"
+      else:
+        text_a = tokenization.convert_to_unicode(line[1])
+        text_b = tokenization.convert_to_unicode(line[2])
+        label = tokenization.convert_to_unicode(line[3])
+      examples.append(
+          InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label))
+    return examples
+
+  
 class SSTProcessor(DataProcessor):
   """Processor for the Stanford Sentiment Treebank data set (GLUE version)."""
 
@@ -819,6 +863,7 @@ def main(_):
       "mrpc": MrpcProcessor,
       "xnli": XnliProcessor,
       "sst": SSTProcessor,
+      "claims": ClaimsProcessor,
   }
 
   tokenization.validate_case_matches_checkpoint(FLAGS.do_lower_case,
