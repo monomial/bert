ef1a74a969103d93342141440142a13c94f799dc
Michael Smith <michaels@miec.com>
2019-02-23 00:18:08 -0800

    now predicting with a threshold

--- run_squad2_predict-Thresh.bat
+++ run_squad2_predict-Thresh.bat
@@ -1,16 +1,19 @@
 set BERT_BASE_DIR=models\uncased_L-12_H-768_A-12
 set SQUAD_DIR=squad11
-set SQUAD_LAPTOP_DIR=tmp\squad_base_laptop
+set SQUAD_OUTPUT_DIR=tmp\squad2_base
+set THRESH=-6.0
 
 python run_squad.py ^
   --vocab_file=%BERT_BASE_DIR%\vocab.txt ^
   --bert_config_file=%BERT_BASE_DIR%\bert_config.json ^
-  --init_checkpoint=%SQUAD_LAPTOP_DIR%\model.ckpt-10859 ^
+  --init_checkpoint=%BERT_BASE_DIR%\bert_model.ckpt ^
   --do_predict=True ^
-  --predict_file=%SQUAD_DIR%\dev-v1.1.json ^
-  --train_batch_size=12 ^
+  --predict_file=%SQUAD_DIR%\dev-v2.0.json ^
+  --train_batch_size=24 ^
   --learning_rate=3e-5 ^
   --num_train_epochs=2.0 ^
   --max_seq_length=384 ^
   --doc_stride=128 ^
-  --output_dir=%SQUAD_LAPTOP_DIR%\
+  --output_dir=%SQUAD_OUTPUT_DIR%\ ^
+  --version_2_with_negative=True ^
+  --null_score_diff_threshold=%THRESH%
